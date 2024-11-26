4253fb8695bbcf6cf807cc283eae137a755dae18
Geoffrey Dupayage <geo_dup@msn.com>
2024-11-26 17:35:48 +0000

    update_local_bike

--- models/mart/mrt_order_monthly_report.sql
+++ models/mart/mrt_order_monthly_report.sql
@@ -2,7 +2,8 @@ SELECT
 order_date_year,
 order_date_month,
 product_id,
-SUM (item_quantity) AS total_quantity
+SUM (item_quantity) AS total_quantity,
+order_date
 FROM {{ ref('int_local_bike__order_items_per_order_date') }}
-GROUP BY 1,2,3
+GROUP BY 1,2,3,5
 ORDER BY 1,2,4
